ad88c8cc4088f02cdc7322e2090e7666627d5da1
bxljoy <259500313@qq.com>
2024-03-09 20:36:27 +0100

    Update file paths and add new company logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,40 +1,42 @@
-import logo from './logo.svg';
-import backend from './backend.png';
-import creator from './creator.png';
-import mobile from './mobile.png';
-import web from './web.png';
-import github from './github.png';
-import menu from './menu.svg';
-import close from './close.svg';
-import logoJ from './letter-j.svg';
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logoJ from "./letter-j.svg";
 
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import mongodb from './tech/mongodb.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import threejs from './tech/threejs.svg';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
 
-import meta from './company/meta.png';
-import shopify from './company/shopify.png';
-import starbucks from './company/starbucks.png';
-import tesla from './company/tesla.png';
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import asiainfo from "./company/asiainfo.png";
+import hdzb from "./company/hdzb.png";
 
-import carrent from './carrent.png';
-import jobit from './jobit.png';
-import tripguide from './tripguide.png';
-import library from './library.png';
-import recipe from './recipe.png';
-import halloween from './halloween.png';
-import hyper from './company/hyper.png';
-import pencilpaws from './pencilpaws.png';
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import library from "./library.png";
+import recipe from "./recipe.png";
+import halloween from "./halloween.png";
+import hyper from "./company/hyper.png";
+import pencilpaws from "./pencilpaws.png";
 
 export {
   logo,
@@ -71,4 +73,6 @@ export {
   halloween,
   hyper,
   pencilpaws,
+  asiainfo,
+  hdzb,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,148 +15,144 @@ import {
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
-  carrent,
-  jobit,
-  tripguide,
   threejs,
+  asiainfo,
+  hdzb,
   library,
   recipe,
   halloween,
-  hyper,
   pencilpaws,
-} from '../assets';
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'work',
-    title: 'Work',
+    id: "work",
+    title: "Work",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Frontend Developer',
+    title: "Frontend Developer",
     icon: web,
   },
   {
-    title: 'React Developer',
+    title: "React Developer",
     icon: mobile,
   },
   {
-    title: 'Backend Developer',
+    title: "Backend Developer",
     icon: backend,
   },
   {
-    title: 'Content Creator',
+    title: "Full Stack Developer",
     icon: creator,
   },
 ];
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
   {
-    name: 'TypeScript',
+    name: "TypeScript",
     icon: typescript,
   },
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Redux Toolkit',
+    name: "Redux Toolkit",
     icon: redux,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: 'Node JS',
+    name: "Node JS",
     icon: nodejs,
   },
   {
-    name: 'MongoDB',
+    name: "MongoDB",
     icon: mongodb,
   },
   {
-    name: 'Three JS',
+    name: "Three JS",
     icon: threejs,
   },
   {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   {
-    name: 'figma',
+    name: "figma",
     icon: figma,
   },
   {
-    name: 'docker',
+    name: "docker",
     icon: docker,
   },
 ];
 
 const experiences = [
   {
-    title: 'Frontend Developer',
-    company_name: 'Hyper Island',
-    icon: hyper,
-    iconBg: '#383E56',
-    date: 'September 2023 - October 2023',
+    title: "Backend Developer",
+    company_name: "AsiaInfo Technologies",
+    icon: asiainfo,
+    iconBg: "#383E56",
+    date: "April 2011 - January 2014",
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      "Engaged in developments of old platform refactor",
+      "Developed backends of a few web applications",
+      "Developed parts of web applications frontends",
+      "Engaged in testing works of web applications",
+      "TroubleShooting and fixing bugs of web applications",
     ],
   },
   {
-    title: 'React.js Developer',
-    company_name: 'Hyper Island',
-    icon: hyper,
-    iconBg: '#383E56',
-    date: 'October 2023 - December 2023',
+    title: "Full Stack Developer",
+    company_name: "HDZB co. Ltd.",
+    icon: hdzb,
+    iconBg: "#383E56",
+    date: "October 2014 - September 2022",
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      "Engaged in products designing and technology selections",
+      "Developed backends of a few web applications",
+      " Developed parts of web applications frontends",
+      "Engaged in developments of internal tool libraries",
+      "TroubleShooting and fixing bugs of web applications",
     ],
   },
   {
-    title: 'Full stack Developer',
-    company_name: 'Hyper Island',
-    icon: hyper,
-    iconBg: '#383E56',
-    date: 'January 2024 - Present',
+    title: "Full stack Developer",
+    company_name: "Freelancer",
+    icon: web,
+    iconBg: "#383E56",
+    date: "January 2023 - Present",
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      "Developing and maintaining web applications using MERN stack and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
 ];
@@ -164,117 +160,117 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      'I thought it was impossible to make a website as beautiful as our product, but Jess proved me wrong.',
-    name: 'Sara Lee',
-    designation: 'CFO',
-    company: 'Acme Co',
-    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+      "I thought it was impossible to make a website as beautiful as our product, but Jess proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Jess does.",
-    name: 'Chris Brown',
-    designation: 'COO',
-    company: 'DEF Corp',
-    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
       "After Jess optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: 'Lisa Wang',
-    designation: 'CTO',
-    company: '456 Enterprises',
-    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: 'Online Library',
+    name: "Online Library",
     description:
-      'Web-based platform that allows users to search, view and buy a wide array of digital resources including ebooks, journals, audio, and video materials.',
+      "Web-based platform that allows users to search, view and buy a wide array of digital resources including ebooks, journals, audio, and video materials.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'green-text-gradient',
+        name: "restapi",
+        color: "green-text-gradient",
       },
       {
-        name: 'bootstrap',
-        color: 'pink-text-gradient',
+        name: "bootstrap",
+        color: "pink-text-gradient",
       },
     ],
     image: library,
-    source_code_link: 'https://github.com/jesslee2023/team-7-react-project',
-    deploy_link: 'https://online-library-jess-react.netlify.app',
+    source_code_link: "https://github.com/jesslee2023/team-7-react-project",
+    deploy_link: "https://online-library-jess-react.netlify.app",
   },
   {
-    name: 'Recipe Finder',
+    name: "Recipe Finder",
     description:
-      'Web application that enables users to search for varities of recipes, view the details for different recipes.',
+      "Web application that enables users to search for varities of recipes, view the details for different recipes.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'green-text-gradient',
+        name: "restapi",
+        color: "green-text-gradient",
       },
       {
-        name: 'bootstrap',
-        color: 'pink-text-gradient',
+        name: "bootstrap",
+        color: "pink-text-gradient",
       },
     ],
     image: recipe,
-    source_code_link: 'https://github.com/jesslee2023/Team6-collaborative-work',
-    deploy_link: 'https://hungry-chief-team6.netlify.app',
+    source_code_link: "https://github.com/jesslee2023/Team6-collaborative-work",
+    deploy_link: "https://hungry-chief-team6.netlify.app",
   },
   {
-    name: 'Hallotween Town',
+    name: "Hallotween Town",
     description:
-      'A website is dedicated to celebrating the spookiest time of the year and showcasing the best Halloween-themed events, activities, and attractions.',
+      "A website is dedicated to celebrating the spookiest time of the year and showcasing the best Halloween-themed events, activities, and attractions.",
     tags: [
       {
-        name: 'html',
-        color: 'blue-text-gradient',
+        name: "html",
+        color: "blue-text-gradient",
       },
       {
-        name: 'css',
-        color: 'green-text-gradient',
+        name: "css",
+        color: "green-text-gradient",
       },
       {
-        name: 'javascript',
-        color: 'pink-text-gradient',
+        name: "javascript",
+        color: "pink-text-gradient",
       },
     ],
     image: halloween,
-    source_code_link: 'https://github.com/jesslee2023/Halloween-town',
-    deploy_link: 'https://halloween-jess.netlify.app',
+    source_code_link: "https://github.com/jesslee2023/Halloween-town",
+    deploy_link: "https://halloween-jess.netlify.app",
   },
   {
-    name: 'Draw and Guess',
-    description: 'A website is dedicated to show the game of Draw and Guess.',
+    name: "Draw and Guess",
+    description: "A website is dedicated to show the game of Draw and Guess.",
     tags: [
       {
-        name: 'html',
-        color: 'blue-text-gradient',
+        name: "html",
+        color: "blue-text-gradient",
       },
       {
-        name: 'css',
-        color: 'green-text-gradient',
+        name: "css",
+        color: "green-text-gradient",
       },
       {
-        name: 'javascript',
-        color: 'pink-text-gradient',
+        name: "javascript",
+        color: "pink-text-gradient",
       },
     ],
     image: pencilpaws,
-    source_code_link: 'https://github.com/jesslee2023/PencilPawsBeta',
-    deploy_link: 'https://jesslee-pencilpaws.netlify.app',
+    source_code_link: "https://github.com/jesslee2023/PencilPawsBeta",
+    deploy_link: "https://jesslee-pencilpaws.netlify.app",
   },
 ];
 
